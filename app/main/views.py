ccc8673cf0db4d0a1b95f825514d2a9839f80dfb
TonyKioko <tonykioko384@gmail.com>
2018-09-09 22:50:20 +0300

    now displaying profile details

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for, abort
 from . import main
 from flask_login import login_required,current_user
 from .forms import PitchesForm,CommentsForm,UpdateProfile
-from ..models import Pitches,Comments
+from ..models import Pitches,Comments,User
 from .. import photos, db
 from datetime import datetime
 
@@ -34,6 +34,45 @@ def home():
     # return "Hello, World"
     return render_template('home.html',title=title,message=message,interview=interview,product=product,promotion=promotion,business=business)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
 @main.route('/pitch/new',methods = ['GET','POST'])
 @login_required
 def new_pitch():
@@ -52,26 +91,6 @@ def new_pitch():
         return redirect(url_for('main.home'))
 
     return render_template('new_pitch.html', pitch_form = form)
-
-# @main.route('/comment/new',methods = ['GET', 'POST'])
-# @login_required
-# def comment():
-#
-#     comments_form = CommentsForm()
-#     # pitch = Pitches.query.get(pitch_id)
-#     # pitches = Pitches.query.filter_by(id=id).first()
-#
-#     if comments_form.validate_on_submit():
-#         comment = comments_form.comment.data
-#
-#         new_comment = Comments(the_comment=comment, user_id = current_user.id)
-#         new_comment.save_comment()
-#
-#         return redirect(url_for('main.home'))
-#     # comments_list = Comments.query.filter_by(pitches_id=pitches.id).all()
-#
-#     return render_template('comments.html', comments_form=comments_form)
-
 
 
 
@@ -93,4 +112,24 @@ def comment(id):
     comments_list = Comments.query.filter_by(pitches_id=pitches.id).all()
 
     return render_template('comments.html', comments_form=comments_form,comments_list=comments_list)
+
+# @main.route('/comment/new',methods = ['GET', 'POST'])
+# @login_required
+# def comment():
+#
+#     comments_form = CommentsForm()
+#     # pitch = Pitches.query.get(pitch_id)
+#     # pitches = Pitches.query.filter_by(id=id).first()
+#
+#     if comments_form.validate_on_submit():
+#         comment = comments_form.comment.data
+#
+#         new_comment = Comments(the_comment=comment, user_id = current_user.id)
+#         new_comment.save_comment()
+#
+#         return redirect(url_for('main.home'))
+#     # comments_list = Comments.query.filter_by(pitches_id=pitches.id).all()
+#
+#     return render_template('comments.html', comments_form=comments_form)
+
 
