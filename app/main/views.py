c8c839d0ddea0930ef0c1562dd6b90ed37951163
TonyKioko <tonykioko384@gmail.com>
2018-09-09 20:21:32 +0300

    fixed macro not defined error

--- app/main/views.py
+++ app/main/views.py
@@ -53,44 +53,44 @@ def new_pitch():
 
     return render_template('new_pitch.html', pitch_form = form)
 
-@main.route('/comment/new',methods = ['GET', 'POST'])
-@login_required
-def comment():
-
-    comments_form = CommentsForm()
-    # pitch = Pitches.query.get(pitch_id)
-    # pitches = Pitches.query.filter_by(id=id).first()
-
-    if comments_form.validate_on_submit():
-        comment = comments_form.comment.data
-
-        new_comment = Comments(the_comment=comment, user_id = current_user.id)
-        new_comment.save_comment()
-
-        return redirect(url_for('main.home'))
-    # comments_list = Comments.query.filter_by(pitches_id=pitches.id).all()
-
-    return render_template('comments.html', comments_form=comments_form)
-
-
-
-
-# @main.route('/pitch/<int:id>',methods = ['GET', 'POST'])
+# @main.route('/comment/new',methods = ['GET', 'POST'])
 # @login_required
-# def comment(id):
+# def comment():
 #
 #     comments_form = CommentsForm()
 #     # pitch = Pitches.query.get(pitch_id)
-#     pitches = Pitches.query.filter_by(id=id).first()
+#     # pitches = Pitches.query.filter_by(id=id).first()
 #
 #     if comments_form.validate_on_submit():
 #         comment = comments_form.comment.data
 #
-#         new_comment = Comments(the_comment=comment,pitches_id=pitches.id, user = current_user)
+#         new_comment = Comments(the_comment=comment, user_id = current_user.id)
 #         new_comment.save_comment()
 #
-#         return redirect(url_for('.comments', pitches_id=pitches.id))
-#     comments_list = Comments.query.filter_by(pitches_id=pitches.id).all()
+#         return redirect(url_for('main.home'))
+#     # comments_list = Comments.query.filter_by(pitches_id=pitches.id).all()
 #
-#     return render_template('comments.html', comments_form=comments_form,comments_list=comments_list)
-#
+#     return render_template('comments.html', comments_form=comments_form)
+
+
+
+
+@main.route('/pitch/<int:id>',methods = ['GET', 'POST'])
+@login_required
+def comment(id):
+
+    comments_form = CommentsForm()
+    # pitch = Pitches.query.get(pitch_id)
+    pitches = Pitches.query.filter_by(id=id).first()
+
+    if comments_form.validate_on_submit():
+        comment = comments_form.comment.data
+
+        new_comment = Comments(the_comment=comment,pitches_id=pitches.id, user_id = current_user.id)
+        new_comment.save_comment()
+
+        return redirect(url_for('main.home'))
+    comments_list = Comments.query.filter_by(pitches_id=pitches.id).all()
+
+    return render_template('comments.html', comments_form=comments_form,comments_list=comments_list)
+
